14b4f434c574bbcc004a0b252e6b8eab9741087f
Nellie K <noircir@users.noreply.github.com>
2019-01-02 23:17:53 -0500

    added absolute path in .env

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+it('shows a comment box', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CommentList).length).toEqual(1);
+});
