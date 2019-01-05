3d93974e3f92807bc8527ee7a9438ec115883b17
Nellie K <noircir@users.noreply.github.com>
2019-01-04 19:52:49 -0500

    testing text in CommentList with Cheerios js

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -1,3 +1,33 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import Root from 'Root';
 
+import CommentList from 'components/CommentList'
+
+let wrapper;
+
+beforeEach(() => {
+    const initialState = {
+        comments: ['Comment 1', 'Comment 2']
+    };
+
+    wrapper = mount(
+        <Root initialState={initialState}>
+            <CommentList />
+        </Root>
+    );
+});
+
+afterEach(() => {
+    wrapper.unmount();
+});
+
+it('creates one LI per comment', ()=> {
+    expect(wrapper.find('li').length).toEqual(2);
+});
+
+it('shows the text for each comment', () => {
+    // console.log(wrapper.render().text());
+    expect(wrapper.render().text()).toContain('Comment 1');
+    expect(wrapper.render().text()).toContain('Comment 2');
+});
